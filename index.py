895af1f2e453a02071a97cd3bcb42e0bcd87924d
Kirill Klimov <ra22341@ya.ru>
2015-05-16 23:08:03 +0400

    first example added

--- index.py
+++ index.py
@@ -5,9 +5,20 @@ import os
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/mysqldb-clean/build/lib.linux-x86_64-2.7/')
 #sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/mysqldb/build/lib.linux-x86_64-2.7/')
 
+from datetime import date
+
 import MySQLdb as mdb
 
 import peewee as pw
+myDB = pw.MySQLDatabase("pract", host="localhost", user="root", passwd="123")
+
+class Person(pw.Model):
+    name = pw.CharField()
+    birthday = pw.DateField()
+    is_relative = pw.BooleanField()
+
+    class Meta:
+        database = myDB
 
 from bottle import route, run, template, request, Bottle
 
@@ -23,11 +34,14 @@ def index(name):
 
         #ver = cur.fetchone()
 
-        myDB = pw.MySQLDatabase("pract", host="localhost", user="root", passwd="123")
         myDB.connect()
+        #Person.create_table()
+        #uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15), is_relative=True)
+        #uncle_bob.save()
+        grandma = Person.select().where(Person.name == 'Bob').get()
 
         return template('templates/index.html', name=str(request.remote_addr),
-                       info='')
+                       info=str(grandma.name))
     except Exception, e:
         return template('templates/index.html', name='', info=(e))
     #return template('<b>Hello {{name}}</b>!', name=name)
