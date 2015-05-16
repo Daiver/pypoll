8a650c102976f179d51307a6c385be2f0cdec4c5
Kirill Klimov <ra22341@ya.ru>
2015-05-16 21:48:07 +0400

    minor changes

--- example4.py
+++ example4.py
@@ -6,11 +6,15 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/mysqldb/build/li
 
 import MySQLdb
 
-from bottle import route, run, template
+from bottle import route, run, template, request, Bottle
 
-@route('/hello/<name>')
+app = Bottle()
+
+#@route('/hello/<name>')
+@route('/<name>')
 def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+    return template('templates/index.html', name=str(request.remote_addr))
+    #return template('<b>Hello {{name}}</b>!', name=name)
 
 if __name__ == '__main__':
     run(server='cgi')
