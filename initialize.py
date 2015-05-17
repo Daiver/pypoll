5bc46170bf60d11be683a8f4d4e9eaba38109dc7
Kirill Klimov <ra22341@ya.ru>
2015-05-17 09:47:26 +0400

    improve .gitignore, and init

--- initialize.py
+++ initialize.py
@@ -4,6 +4,10 @@ from datetime import date
 
 if __name__ == '__main__':
     Model.myDB.connect()
-    Model.Person.create_table()
-    grandMa = Person(name='Grand', birthday=date(1970, 1, 1), is_relative=True)
+    try:
+        Model.Person.create_table()
+    except Exception as e:
+        print 'Cannot create table'
+        print e
+    grandMa = Model.Person(name='Grand', birthday=date(1970, 1, 1), is_relative=True)
     grandMa.save()
