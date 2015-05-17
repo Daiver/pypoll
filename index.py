4641e2b3602398850f1d03ffa20b572f694eeb22
daiver <ra22341@ya.ru>
2015-05-17 15:40:26 +0300

    minor changes

--- index.py
+++ index.py
@@ -11,7 +11,7 @@ import random
 
 import MySQLdb as mdb
 
-from Model import myDB, Person, Poll, PollItem, PollVote
+from Model import myDB, Poll, PollItem, PollVote
 
 from bottle import route, run, template, request, Bottle, post, redirect, response, error, abort
 
@@ -28,31 +28,6 @@ def formtest():
 def formtest():
     return template('templates/form.html', text=request.forms.get('name'))
 
-#@route('/hello/<name>')
-#@route('/<name>')
-#def index1(name):
-    #try:
-        ##con = mdb.connect('localhost', 'root', '123', 'pract');
-        ##cur = con.cursor()
-        ##cur.execute("SELECT VERSION()")
-
-        ##ver = cur.fetchone()
-
-        #myDB.connect()
-        ##Person.create_table()
-        ##uncle_bob = Person(name='Bob', birthday=date(1960, 1, 15), is_relative=True)
-        ##uncle_bob.save()
-        #grandma = Person.select().where(Person.name == 'Grand').get()
-
-        #return template('templates/index.html', name=str(request.remote_addr),
-                       #info=str(grandma.name))
-    #except Exception, e:
-        #return template('templates/index.html', name='', info=(e))
-    ##return template('<b>Hello {{name}}</b>!', name=name)
-
-#@error(404)
-#def error404():
-    #return template('templates/404.html')
 
 #hostname = "http://192.168.10.101/cgi-bin/pypoll.py"
 
