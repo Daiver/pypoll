c872209fd7a7854f8a073f00a00ee48ead0e37e7
daiver <ra22341@ya.ru>
2015-05-17 13:11:25 +0300

    add separate 404 handler

--- index.py
+++ index.py
@@ -50,10 +50,13 @@ def index1(name):
         return template('templates/index.html', name='', info=(e))
     #return template('<b>Hello {{name}}</b>!', name=name)
 
+@error(404)
+def process404():
+    return template('templates/404.html')
+
 @route('/poll/<url>')
 def poll(url):
-    return template('templates/404.html')
-    #abort(404, "No such poll")
+    abort(404, "No such poll")
 
 @post('/newpoll')
 def newpoll():
