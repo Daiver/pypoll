f1e3e3a5d1e3905be32bb4ea7e43e29a285a25a8
daiver <ra22341@ya.ru>
2015-06-06 15:51:29 +0300

    minor changes

--- initialize.py
+++ initialize.py
@@ -14,14 +14,15 @@ if __name__ == '__main__':
         ver = cur.fetchone()
                         
         print "Database version : %s " % ver
-
-        cur.execute("DROP TABLE pollvote;")
-        cur.execute("DROP TABLE pollitem;")
-        cur.execute("DROP TABLE POLL;")
-
     except Exception as e:
         print "Some err on direct connect"
         print e
+        exit(1)
+
+    cur.execute("DROP TABLE pollvote;")
+    cur.execute("DROP TABLE pollitem;")
+    cur.execute("DROP TABLE poll;")
+
 
     Model.myDB.connect()
 
@@ -42,6 +43,13 @@ if __name__ == '__main__':
     except Exception as e:
         print 'Cannot create table'
         print e
+
+    con.execute("ALTER TABLE pollvote CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;")
+    con.execute("ALTER TABLE pollitem CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;")
+    con.execute("ALTER TABLE poll CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;")
+
+    #ALTER TABLE tablename CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
 
 #    grandMa = Model.Person(name='Grand', birthday=date(1970, 1, 1), is_relative=True)
 #    grandMa.save()
